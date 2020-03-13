c1b9f5119df2b83fbc0644dbfb060e73c16f0f90
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2020-03-13 21:03:33 +0100

    Updates

---
+++ precache-manifest.b820fbabf7cfbaa041e5740ef87830cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023ef8d9aabb7108ee9387349b7f7e69",
+    "url": "/CrossPWAGame/index.html"
+  },
+  {
+    "revision": "110f60a77c611f212cb9",
+    "url": "/CrossPWAGame/static/js/2.46ae379f.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/CrossPWAGame/static/js/2.46ae379f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a162f1be2fe4402545e2",
+    "url": "/CrossPWAGame/static/js/main.2e01a204.chunk.js"
+  },
+  {
+    "revision": "d46e8fd943a41e4e3307",
+    "url": "/CrossPWAGame/static/js/runtime-main.5f8cb4db.js"
+  }
+]);
