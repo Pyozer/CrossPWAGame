861ee17e9ced75e4be78161a41420f227943b654
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2020-03-17 09:56:25 +0100

    Updates

---
+++ precache-manifest.e79bf12825879ff9886e503c6f3b22bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb0ecb836fd212455d64ad0f7c6c84e",
+    "url": "/CrossPWAGame/index.html"
+  },
+  {
+    "revision": "a501d184ee775a935597",
+    "url": "/CrossPWAGame/static/js/2.804f7540.chunk.js"
+  },
+  {
+    "revision": "5c2176e7db1837929debf21c65634f23",
+    "url": "/CrossPWAGame/static/js/2.804f7540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27dd2b3aea748cd3597b",
+    "url": "/CrossPWAGame/static/js/main.a322da53.chunk.js"
+  },
+  {
+    "revision": "d46e8fd943a41e4e3307",
+    "url": "/CrossPWAGame/static/js/runtime-main.5f8cb4db.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CrossPWAGame/precache-manifest.427bcda3c3d922d13be8f3da01ee3740.js"
+  "/CrossPWAGame/precache-manifest.e79bf12825879ff9886e503c6f3b22bb.js"
 );
 
 self.addEventListener('message', (event) => {
